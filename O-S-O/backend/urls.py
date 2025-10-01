374ff01b812ab213aaabbc75db29fe11fee38da3
Omer-sanee <omersanee470@gmail.com>
2025-10-01 07:54:11 -1100

    Update urls.py

--- O-S-O/backend/urls.py
+++ O-S-O/backend/urls.py
@@ -21,8 +21,9 @@ from backend import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path(", views.home, name='home'),  
+    path("", views.home, name='home'),  
 ]
+
 
 
 
